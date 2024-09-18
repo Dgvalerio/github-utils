3045dbd8612982efddc1a14c458b764116ae4435
davi.valerio <al.davigvalerio@gmail.com>
2024-09-18 16:15:02 -0300

    feat(pull-requests): add loading to pulls, split page in components and add pulls reload button

--- src/app/(private)/pull-requests/page.tsx
+++ src/app/(private)/pull-requests/page.tsx
@@ -15,6 +15,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
+import { Skeleton } from '@/components/ui/skeleton';
 import {
   loadGithubPullRequests,
   loadGithubRepositories,
@@ -25,10 +26,47 @@ import { githubToView } from '@/utils/mappers/github-to-view';
 import { AlertCircle, ExternalLink, Plus, X } from 'lucide-react';
 import htmlRender from 'rehype-raw';
 
-const PullRequestsView: FC<{
-  repositories: View.Repository['fullName'][];
-}> = ({ repositories }) => {
+const RepositoriesView: FC = () => {
+  const { repositories: selectedRepositories, removeRepository } =
+    usePullRequestsStore();
+
+  const handleRemoveRepository = (repositoryName: string): void => {
+    void removeRepository(repositoryName);
+  };
+
+  if (selectedRepositories.length === 0)
+    return (
+      <h2 className="flex flex-wrap gap-2 font-light">
+        <AlertCircle /> Selecione um repositório para visualizar os Pull
+        Requests
+      </h2>
+    );
+
+  return (
+    <div className="flex flex-col items-center justify-between gap-2">
+      <h2 className="font-bold">Visualizando pull requests de:</h2>
+      <div className="flex flex-wrap gap-2">
+        {selectedRepositories.map((repository) => (
+          <Badge className="gap-2 pr-1" key={repository}>
+            {repository}
+            <Button
+              variant="ghost"
+              size="icon"
+              className="h-auto w-auto p-0"
+              onClick={handleRemoveRepository.bind(null, repository)}
+            >
+              <X className="h-4 w-4" />
+            </Button>
+          </Badge>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const PullRequestsView: FC = () => {
   const { data } = useSession();
+  const { repositories } = usePullRequestsStore();
 
   const [loading, setLoading] = useState(true);
   const [pullRequests, setPullRequests] = useState<View.PullRequest[]>([]);
@@ -52,84 +90,97 @@ const PullRequestsView: FC<{
     void loadPullRequests();
   }, [loadPullRequests]);
 
-  if (loading) return <>Loading...</>;
-
   return (
-    <div className="grid grid-cols-2 gap-4 xl:grid-cols-3">
-      {pullRequests.length > 0 &&
-        pullRequests.map((pull) => (
-          <div
-            key={pull.title}
-            className="flex flex-col gap-2 rounded border p-4"
-          >
-            <div className="flex flex-wrap items-center justify-between gap-2">
-              <div className="flex items-center gap-2">
-                <Avatar className="h-8 w-8">
-                  <AvatarImage src={pull.user.avatar} />
-                  <AvatarFallback>
-                    {getAvatarFallback(pull.user.login)}
-                  </AvatarFallback>
-                </Avatar>
-                <span className="mr-auto truncate">{pull.user.login}</span>
-              </div>
-              <div className="ml-auto flex items-center justify-end gap-2">
-                <span className="text-foreground">{pull.createdAt}</span>
-                <Button size="icon" variant="outline" asChild>
-                  <Link href={pull.url} target="_blank">
-                    <ExternalLink className="h-4 w-4" />
-                  </Link>
-                </Button>
-              </div>
-            </div>
-            <Separator />
-            <div className="flex items-center justify-between gap-2">
-              <h1 className="text-2xl font-bold">{pull.title}</h1>
-              <Badge
-                className="h-8 border-green-600 text-base text-green-600"
-                variant="outline"
-              >
-                {pull.state}
-              </Badge>
-            </div>
-            <Separator />
-            <Markdown
-              rehypePlugins={[htmlRender]}
-              components={{
-                h1: ({ children }): ReactElement => (
-                  <h1 className="text-2xl">{children}</h1>
-                ),
-                h2: ({ children }): ReactElement => (
-                  <h2 className="text-xl">{children}</h2>
-                ),
-                h3: ({ children }): ReactElement => (
-                  <h3 className="text-lg">{children}</h3>
-                ),
-                p: ({ children }): ReactElement => <p>{children}</p>,
-                a: ({ children, href }): ReactElement => (
-                  <Button asChild variant="link">
-                    <Link href={href || '#'} target="_blank">
-                      {children}
+    <>
+      <div className="flex items-stretch gap-2">
+        <Button
+          size="icon"
+          className="h-auto"
+          disabled={loading}
+          onClick={loadPullRequests}
+        >
+          <ReloadIcon className="h-4 w-4" />
+        </Button>
+        <RepositoriesView />
+      </div>
+      <Separator />
+      <div className="grid grid-cols-2 gap-4 xl:grid-cols-3">
+        {loading &&
+          [...new Array(6)].map((_, index) => (
+            <Skeleton key={index} className="min-h-32 w-full" />
+          ))}
+        {!loading &&
+          pullRequests.length > 0 &&
+          pullRequests.map((pull) => (
+            <div
+              key={pull.title}
+              className="flex flex-col gap-2 rounded border p-4"
+            >
+              <div className="flex flex-wrap items-center justify-between gap-2">
+                <div className="flex items-center gap-2">
+                  <Avatar className="h-8 w-8">
+                    <AvatarImage src={pull.user.avatar} />
+                    <AvatarFallback>
+                      {getAvatarFallback(pull.user.login)}
+                    </AvatarFallback>
+                  </Avatar>
+                  <span className="mr-auto truncate">{pull.user.login}</span>
+                </div>
+                <div className="ml-auto flex items-center justify-end gap-2">
+                  <span className="text-foreground">{pull.createdAt}</span>
+                  <Button size="icon" variant="outline" asChild>
+                    <Link href={pull.url} target="_blank">
+                      <ExternalLink className="h-4 w-4" />
                     </Link>
                   </Button>
-                ),
-              }}
-            >
-              {pull.body}
-            </Markdown>
-          </div>
-        ))}
-    </div>
+                </div>
+              </div>
+              <Separator />
+              <div className="flex items-center justify-between gap-2">
+                <h1 className="text-2xl font-bold">{pull.title}</h1>
+                <Badge
+                  className="h-8 border-green-600 text-base text-green-600"
+                  variant="outline"
+                >
+                  {pull.state}
+                </Badge>
+              </div>
+              <Separator />
+              <Markdown
+                rehypePlugins={[htmlRender]}
+                components={{
+                  h1: ({ children }): ReactElement => (
+                    <h1 className="text-2xl">{children}</h1>
+                  ),
+                  h2: ({ children }): ReactElement => (
+                    <h2 className="text-xl">{children}</h2>
+                  ),
+                  h3: ({ children }): ReactElement => (
+                    <h3 className="text-lg">{children}</h3>
+                  ),
+                  p: ({ children }): ReactElement => <p>{children}</p>,
+                  a: ({ children, href }): ReactElement => (
+                    <Button asChild variant="link">
+                      <Link href={href || '#'} target="_blank">
+                        {children}
+                      </Link>
+                    </Button>
+                  ),
+                }}
+              >
+                {pull.body}
+              </Markdown>
+            </div>
+          ))}
+      </div>
+    </>
   );
 };
 
-const PullRequestsPage: NextPage = () => {
+const RepositoriesSet: FC = () => {
   const { data } = useSession();
 
-  const {
-    repositories: selectedRepositories,
-    addRepository,
-    removeRepository,
-  } = usePullRequestsStore();
+  const { addRepository } = usePullRequestsStore();
 
   const [repositories, setRepositories] = useState<View.Repository[]>([]);
   const [repository, setRepository] = useState<string>();
@@ -156,75 +207,44 @@ const PullRequestsPage: NextPage = () => {
     void addRepository(repository);
   };
 
-  const handleRemoveRepository = (repositoryName: string): void => {
-    void removeRepository(repositoryName);
-  };
-
   useEffect(() => {
     void loadRepositories();
   }, [loadRepositories]);
 
   return (
-    <main className="flex flex-col gap-4">
-      <div className="flex items-center justify-between">
-        <h1 className="text-lg font-semibold">Pull Requests</h1>
-        <Button onClick={loadRepositories} className="gap-2" disabled={loading}>
-          <ReloadIcon /> Recarregar
-        </Button>
-      </div>
-      <Separator />
-      <div className="flex items-center gap-2">
-        <Combobox
-          loading={loading}
-          placeholder="Repositórios"
-          containerClassName="flex-1 w-full"
-          handleSelect={setRepository}
-          items={repositories.map((repository) => ({
-            value: repository.fullName,
-            label: repository.fullName,
-          }))}
-        />
-        <Button
-          onClick={handleAddRepository}
-          className="gap-2"
-          disabled={loading}
-        >
-          <Plus /> Adicionar
-        </Button>
-      </div>
-      <Separator />
-      {selectedRepositories.length > 0 && (
-        <>
-          <h2 className="font-bold">Visualizando pull requests de:</h2>
-          <div className="flex flex-wrap gap-2">
-            {selectedRepositories.map((repository) => (
-              <Badge className="gap-2 pr-1" key={repository}>
-                {repository}
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="h-auto w-auto p-0"
-                  onClick={handleRemoveRepository.bind(null, repository)}
-                >
-                  <X className="h-4 w-4" />
-                </Button>
-              </Badge>
-            ))}
-          </div>
-        </>
-      )}
-      {selectedRepositories.length === 0 && (
-        <h2 className="flex flex-wrap gap-2 font-light">
-          <AlertCircle /> Selecione um repositório para visualizar os Pull
-          Requests
-        </h2>
-      )}
-      <Separator />
-      <PullRequestsView
-        repositories={selectedRepositories.map((repository) => repository)}
+    <div className="flex items-center gap-2">
+      <Button onClick={loadRepositories} size="icon" disabled={loading}>
+        <ReloadIcon />
+      </Button>
+      <Combobox
+        loading={loading}
+        placeholder="Repositórios"
+        containerClassName="flex-1 w-full"
+        handleSelect={setRepository}
+        items={repositories.map((repository) => ({
+          value: repository.fullName,
+          label: repository.fullName,
+        }))}
       />
-    </main>
+      <Button
+        onClick={handleAddRepository}
+        className="gap-2"
+        disabled={loading}
+      >
+        <Plus /> Adicionar
+      </Button>
+    </div>
   );
 };
+
+const PullRequestsPage: NextPage = () => (
+  <main className="flex flex-col gap-4">
+    <h1 className="text-lg font-semibold">Pull Requests</h1>
+    <Separator />
+    <RepositoriesSet />
+    <Separator />
+    <PullRequestsView />
+  </main>
+);
 
 export default PullRequestsPage;
